4886dce272720e365e9f32066d3e6d4f3dbf250c
yu <yurimkang20@gmail.com>
2025-11-08 20:58:21 +0900

    not solved: [백준] 2941. 크로아티아 알파벳 (Python)

---
+++ 02 백준/02 구현/Python/Prob2941.py
@@ -0,0 +1,18 @@
+
+"""
+title  : 2941. 크로아티아 알파벳 (Silver 5)
+time   : ms   ms
+memory : KB   KB
+"""
+
+import sys
+
+def solution():
+    input = sys.stdin.readline
+
+    N = input()
+
+    
+
+solution()
+
